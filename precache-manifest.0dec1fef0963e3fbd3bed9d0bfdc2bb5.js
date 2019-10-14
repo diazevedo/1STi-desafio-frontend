60fd0ed1abdae44053fc982396ea04ebf35507e4
Diego Azevedo <dapptn@hotmail.com>
2019-10-14 14:19:28 +1100

    Updates

---
+++ precache-manifest.0dec1fef0963e3fbd3bed9d0bfdc2bb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cea941ae401c9d8e86b7bde5882f3583",
+    "url": "/forecast/index.html"
+  },
+  {
+    "revision": "3988a90e66ffe6d1bfee",
+    "url": "/forecast/static/css/main.4e4fbf93.chunk.css"
+  },
+  {
+    "revision": "6a36713398102f7d0720",
+    "url": "/forecast/static/js/2.c78e47bb.chunk.js"
+  },
+  {
+    "revision": "3988a90e66ffe6d1bfee",
+    "url": "/forecast/static/js/main.b0e95f38.chunk.js"
+  },
+  {
+    "revision": "0a580239fe3d5607ccd4",
+    "url": "/forecast/static/js/runtime~main.1c70b54c.js"
+  }
+]);
