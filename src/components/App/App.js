b0fe2d366bfd1807145703c5f74d0b790893503c
Diego Azevedo <dapptn@hotmail.com>
2019-10-15 21:09:35 +1100

    Request response validation

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,58 +1,72 @@
-import React, { useState } from 'react'
-import Header from '../Header'
-import FormSearchCity from '../FormSearchCity'
-import SectionCitie from "../SectionCities"
-import SectionCityWeather from "../SectionCityWeather"
+import React, { useState } from "react";
+import Header from "../Header";
+import FormSearchCity from "../FormSearchCity";
+import SectionCitie from "../SectionCities";
+import SectionCityWeather from "../SectionCityWeather";
 
-const { api } = require ("../../helpers/apiCall")
-const { conditions } = require ("../../helpers/conditions")
-const { cityObject } = require ("../../helpers/cityObject")
+const { api } = require("../../helpers/apiCall");
+const { conditions } = require("../../helpers/conditions");
+const { cityObject } = require("../../helpers/cityObject");
 
 const App = () => {
-
   const [cityCurrentWeather, setCityCurrentWeather] = useState({});
 
   const handleClickSearch = (event, citySearchedName) => {
-    
     event.preventDefault();
 
-    if(citySearchedName.length === 0)
-      return;
+    if (citySearchedName.length === 0) return;
 
     api.get(
       `https://weather-ydn-yql.media.yahoo.com/forecastrss?location=${citySearchedName}&format=json&u=c`,
       null,
       null,
-      function (err, data, result) {
-        let forescast = cityObject(JSON.parse(data));
-        forescast.currentCondition.condition = conditions(forescast.currentCondition.condition);
-        setCityCurrentWeather(forescast);
-      }
-    )
-  }
+      function(err, data, result) {
+        const response = JSON.parse(data);
+        if (Object.entries(response.location).length > 0) {
+          let forescast = cityObject(response);
 
-  const handleClickClose = (event) => {
+          forescast.currentCondition.condition = conditions(
+            forescast.currentCondition.condition
+          );
+
+          setCityCurrentWeather(forescast);
+        }
+      }
+    );
+  };
+
+  const handleClickClose = event => {
     event.preventDefault();
     setCityCurrentWeather({});
-  }
+  };
 
   return (
     <>
-      <Header classModifier={`${Object.keys(cityCurrentWeather).length > 0 ? "-smaller" : ""}`}/>
-      
-      <SectionCityWeather 
-        currentWeather={cityCurrentWeather} 
+      <Header
+        classModifier={`${
+          Object.keys(cityCurrentWeather).length > 0 ? "-smaller" : ""
+        }`}
+      />
+
+      <SectionCityWeather
+        currentWeather={cityCurrentWeather}
         handleClick={handleClickClose}
       />
 
-      <FormSearchCity 
+      <FormSearchCity
         handleClick={handleClickSearch}
-        classModifier={`${Object.keys(cityCurrentWeather).length > 0 ? "-smaller" : ""}`}  
+        classModifier={`${
+          Object.keys(cityCurrentWeather).length > 0 ? "-smaller" : ""
+        }`}
       />
 
-      <SectionCitie classModifier={`${Object.keys(cityCurrentWeather).length > 0 ? "-smaller" : ""}`}/>
+      <SectionCitie
+        classModifier={`${
+          Object.keys(cityCurrentWeather).length > 0 ? "-smaller" : ""
+        }`}
+      />
     </>
-  )
-}
+  );
+};
 
 export default App;
--- src/components/TableCities/index.js
+++ src/components/TableCities/index.js
@@ -1,29 +1,26 @@
-import React from "react"
-import "./styles.css"
+import React from "react";
+import "./styles.css";
 
-const TableCities = ({cities}) => {
+const TableCities = ({ cities }) => {
+  return (
+    <table className="table">
+      <thead className="thead">
+        <tr>
+          <th>Min</th>
+          <th>Máx</th>
+        </tr>
+      </thead>
+      <tbody className="tbody-tsi">
+        {cities.map(({ min, max, name }, index) => (
+          <tr key={index}>
+            <td className="td-min">{min}</td>
+            <td>{max}</td>
+            <td>{name}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
-     return (
-        <table className="table">
-          <thead className="thead">
-            <tr>
-              <th>Min</th>
-              <th>Máx</th>
-            </tr>
-          </thead>
-          <tbody className="tbody-tsi">
-          {
-            cities.map(({min, max, name}, index) =>
-            <tr key={index}>
-              <td className="td-min">{min}</td>
-              <td>{max}</td>
-              <td>{name}</td>
-            </tr>
-            )  
-          }
-          </tbody>
-        </table>
-    )
-}
-
-export default TableCities
+export default TableCities;
